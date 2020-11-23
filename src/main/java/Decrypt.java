5431881e41f0b7e9d173d496dcbf31c71da42e53
Emmanuel1738 <emmanuelbett13@gmail.com>
2020-11-23 13:01:15 +0300

    coded Decrypt.java

--- src/main/java/Decrypt.java
+++ src/main/java/Decrypt.java
@@ -1,2 +1,25 @@
+
+package CaesarCipher;
 public class Decrypt {
+    public static StringBuffer decryptClass(String decryptMessage, int decryptShift) {
+        StringBuffer result = new StringBuffer();
+        if (decryptShift > 26) {
+            decryptShift = decryptShift % 26;
+        } else if (decryptShift < 0) {
+            decryptShift = (decryptShift % 26) + 26;
+        }
+        for (int i = 0; i < decryptMessage.length(); i++) {
+            if (Character.isUpperCase(decryptMessage.charAt(i))) {
+                char character = (char) (((int) decryptMessage.charAt(i) -
+                        decryptShift - 65) % 26 + 65);
+                result.append(character);
+            } else {
+                char character = (char) (((int) decryptMessage.charAt(i) -
+                        decryptShift - 97) % 26 + 97);
+                result.append(character);
+            }
+        }
+        return result;
+    }
+
 }
